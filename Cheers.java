489ef38a2a17b5d3b67ff9fba963637f1294b711
shachaf kuyumgiski <shachaf.kuyumgiski@post.runi.ac.il>
2025-11-04 20:57:01 +0200

    first try

--- Cheers.java
+++ Cheers.java
@@ -1,13 +1,34 @@
-//feedback
-
-/*
-Great work boaz!
-*/
-
 
 // Prints a crowd cheering output.
 public class Cheers {
         public static void main(String[] args) {
-	    // Replace this comment with your code
+                int reps = Integer.parseInt(args[1]);
+                String name = args[0];
+                String upper_name = name.toUpperCase();
+                String an = "AEFHILMNORSX";
+                int len = name.length();
+                int c = 0;
+                while (c < len) 
+                {
+                        char letter = upper_name.charAt(c);
+                        if (an.indexOf(letter) != -1)
+                        {
+                                System.out.println("Give me an " + letter + ": " + letter + "!");
+                        } 
+                        else
+                        {
+                                System.out.println("Give me a " + letter + ": " + letter + "!");
+                        }
+                        c = c + 1;
+                }
+                System.out.println("What does that spell?");
+                int c2 = 0;
+                while (c2 < reps)
+                {
+                        System.out.println(upper_name + "!!!");
+                        c2 = c2 + 1;
+                }
+        
+
         }
 }
